ef13cf4f68a2d3415f37c99afa1035b155855ed1
pkprabu <pkprabu07@gmail.com>
2020-01-08 13:28:41 +0530

    1 file modified

--- src/app/custom-validators/cannot-contain-space.ts
+++ src/app/custom-validators/cannot-contain-space.ts
@@ -2,11 +2,11 @@ import { AbstractControl, ValidationErrors } from '@angular/forms'
 
 export class CustomValidators {
   private cannotContainSpace(control: AbstractControl): ValidationErrors | null {
-    // if((control.value as string).indexOf(' ') >= 0) {
-    //   return {
-    //     cannotContainSpace: true
-    //   }
-    // }
-    // return null;
+    if((control.value as string).indexOf(' ') >= 0) {
+      return {
+        cannotContainSpace: true
+      }
+    }
+    return null;
   }
 }
